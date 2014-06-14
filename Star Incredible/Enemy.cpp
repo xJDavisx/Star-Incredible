85267265d2b4776f2d8cdf36956411e1861a5cc3
Jesse <jessedavis10031992@gmail.com>
2014-06-13 23:47:37 -0400

    Empty Enemy Class added - Has no Funtionality

---
+++ Star Incredible/Enemy.cpp
@@ -0,0 +1,22 @@
+/*
+	**************************************************************************************************************************
+	Class:		Enemy
+	Purpose:	To add "Bad-guy" or "Enemy" functionality to the game. This class will encapsulate a basic "Galaga" like
+				movement and approach.
+	Version:	0.0.0.1
+	Author:		Jesse Davis
+	Date:		6/13/2014
+	**************************************************************************************************************************
+*/
+
+#include "Enemy.h"
+
+
+Enemy::Enemy()
+{
+}
+
+
+Enemy::~Enemy()
+{
+}
