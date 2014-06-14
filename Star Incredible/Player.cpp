93fe8b6c0cef5022644b96761aca4f131ea14c6d
Frank Wylupek <fwylupek@gmail.com>
2014-06-13 21:59:48 -0400

    new standard

--- Star Incredible/Player.cpp
+++ Star Incredible/Player.cpp
@@ -1,20 +1,18 @@
-// Player.cpp
-
 #include "Player.h"
 
 Player::Player(SDLGraphics* graphics,
     int imageX, int imageY,
     int width, int height,
-    int transparentR, int transparentG, int transparentB,
     const char* bitmapFileName,
     float x, float y,
-    float maxSpeed) : m_graphics(graphics),
+    float maxSpeed, Input* input) : m_graphics(graphics),
     m_imageX(imageX), m_imageY(imageY),
     m_width(width), m_height(height),
     m_x(x), m_y(y),
     m_maxSpeed(maxSpeed),
     m_currentSpeedX(0.0f){
 
+    g_input = input;
     m_bitmap = m_graphics->loadPNG(bitmapFileName);
 
 }
@@ -27,7 +25,10 @@ Player::~Player(){
 
 void Player::update(float deltaTime){
 
+    handleKeyboardInput();
     m_x += m_currentSpeedX * deltaTime;
+    m_y += m_currentSpeedY * deltaTime;
+    draw();
 
 }
 
@@ -51,9 +52,59 @@ void Player::moveRight(){
     m_currentSpeedX = m_maxSpeed;
 
 }
+void Player::moveUp(){
+
+    m_currentSpeedY = -m_maxSpeed;
+
+}
+
+void Player::moveDown(){
+
+    m_currentSpeedY = m_maxSpeed;
+
+}
 
 void Player::stopMoving(){
 
     m_currentSpeedX = 0.0f;
+    m_currentSpeedY = 0.0f;
+
+}
+
+void Player::handleKeyboardInput(){
+
+    bool* keysHeld = g_input->getInput();
+    bool isKeyHeld = false;
+
+    if (keysHeld[SDLK_LEFT]){
+
+        moveLeft();
+        isKeyHeld = true;
+
+    }
+    if (keysHeld[SDLK_UP]){
+
+        moveUp();
+        isKeyHeld = true;
+
+    }
+    if (keysHeld[SDLK_DOWN]){
+
+        moveDown();
+        isKeyHeld = true;
+
+    }
+
+    if (keysHeld[SDLK_RIGHT]){
+
+        moveRight();
+        isKeyHeld = true;
+
+    }
+    if (!isKeyHeld){
+
+        stopMoving();
+
+    }
 
 }
