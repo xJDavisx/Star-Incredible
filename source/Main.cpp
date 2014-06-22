40825e15ecfce3b8974f7c36257c0b2d372f284d
Frank Wylupek <fwylupek@gmail.com>
2014-06-22 19:11:40 -0400

    experimental levelOne() function

--- source/Main.cpp
+++ source/Main.cpp
@@ -13,47 +13,66 @@
 
 using namespace std;
 
-const char *WINDOW_TITLE = "Star Incredible";
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 600;
-
-const char *PLAYER_SHIP_FILENAME = "resources/player-ship1.png";
-const float PLAYER_START_X = 0;
-const float PLAYER_START_Y = 500;
-const int PLAYER_IMAGE_X = 0;
-const int PLAYER_IMAGE_Y = 0;
-const int PLAYER_IMAGE_WIDTH = 50;
-const int PLAYER_IMAGE_HEIGHT = 100;
-const float PLAYER_SPEED = 600.0;
-
-const char *ENEMY_SHIP_FILENAME = "resources/enemy-ship.png";
-const int ENEMY_IMAGE_X = 0;
-const int ENEMY_IMAGE_Y = 0;
-const int ENEMY_IMAGE_WIDTH = 50;
-const int ENEMY_IMAGE_HEIGHT = 100;
-const int ENEMY_SPEED = 10;
-const int ENEMY_CAPACITY = 50;
-
 bool g_gameIsRunning = true;
-Player *player_ship = NULL;
-SDLGraphics *g_graphics = NULL;
-Input *g_input = NULL;
-Timer *g_timer = NULL;
-Enemy *enemies[ENEMY_CAPACITY];
+
+Input *g_input = new Input();
 
 void handleKeyboardInput();
+void levelOne();
 
 int main(int argc, char *args[])
 {
+    levelOne();
+
+    return 0;
+}
+
+void handleKeyboardInput()
+{
+    bool* keysHeld = g_input->getInput();
+
+    if (keysHeld[SDLK_ESCAPE])
+    {
+        g_gameIsRunning = false;
+    }
+}
+
+void levelOne()
+{
+    const char *WINDOW_TITLE = "Star Incredible";
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 600;
+
+    const char *PLAYER_SHIP_FILENAME = "resources/player-ship1.png";
+    const float PLAYER_START_X = 0;
+    const float PLAYER_START_Y = 500;
+    const int PLAYER_IMAGE_X = 0;
+    const int PLAYER_IMAGE_Y = 0;
+    const int PLAYER_IMAGE_WIDTH = 50;
+    const int PLAYER_IMAGE_HEIGHT = 100;
+    const float PLAYER_SPEED = 600.0;
+
+    const char *ENEMY_SHIP_FILENAME = "resources/enemy-ship.png";
+    const int ENEMY_IMAGE_X = 0;
+    const int ENEMY_IMAGE_Y = 0;
+    const int ENEMY_IMAGE_WIDTH = 50;
+    const int ENEMY_IMAGE_HEIGHT = 100;
+    const int ENEMY_SPEED = 10;
+    const int ENEMY_CAPACITY = 50;
+
+    Player *player_ship = NULL;
+    SDLGraphics *g_graphics = NULL;
+    Timer *g_timer = NULL;
+    Enemy *enemies[ENEMY_CAPACITY];
+
     //set array of enemies
     for (int i = 0; i < ENEMY_CAPACITY + 1; i++)
     {
         enemies[i] = new Enemy[ENEMY_CAPACITY + 1];
     }
 
-    g_graphics = new SDLGraphics(800, 600, WINDOW_TITLE);
+    g_graphics = new SDLGraphics(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     g_graphics->setBackground("resources/background.png");
-    g_input = new Input();
 
     player_ship = new Player(g_graphics,
                              PLAYER_IMAGE_X, PLAYER_IMAGE_Y,
@@ -125,16 +144,4 @@ int main(int argc, char *args[])
     }
 
     delete g_graphics;
-
-    return 0;
-}
-
-void handleKeyboardInput()
-{
-    bool* keysHeld = g_input->getInput();
-
-    if (keysHeld[SDLK_ESCAPE])
-    {
-        g_gameIsRunning = false;
-    }
 }
